913157b7e39f1e51f0bfe26bee40e8b84ff25573
Marostega <103620713+marostegaf@users.noreply.github.com>
2024-02-05 11:49:19 -0300

    v1.0

---
+++ app.js
@@ -0,0 +1,29 @@
+function imgSlider(anything) {
+    document.querySelector(".starbucks").src = anything;
+}
+
+function changeColors(color) {
+    const circle = document.querySelector(".circle");
+    circle.style.background = color;
+
+    const starbucksText = document.querySelector(".content .textBox h2 span");
+    starbucksText.style.color = color;
+
+    const lerMaisButton = document.querySelector(".content .textBox a");
+    lerMaisButton.style.backgroundColor = color;
+    lerMaisButton.style.borderColor = color;
+
+    const agradecimento = document.querySelector(".lermais__agradecimento span");
+    agradecimento.style.color = color;
+
+    const header = document.querySelector(".circle ul li a");
+    header.style.color = color;
+
+}
+
+function exibirParagrafo() {
+    var paragrafo = document.querySelector(".lermais__agradecimento");
+    var lerMaisLink = document.querySelector(".lermais__link");
+    paragrafo.style.display = "block";
+    lerMaisLink.style.display = "none";
+}
